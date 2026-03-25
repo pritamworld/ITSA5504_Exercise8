77e16c61e0d3858ea03cf81d6860ab41fcdac307
Pritesh Patel <pritam.world@gmail.com>
2026-03-25 10:38:14 -0400

    Add greet endpoint to return welcome message

--- app/app.py
+++ app/app.py
@@ -10,5 +10,9 @@ def health():
 def hello():
     return jsonify(message="hello, world"), 200
 
+@app.route("/greet")
+def greet():
+    return jsonify(message="Welcome to Docker and GitHub Action"), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
